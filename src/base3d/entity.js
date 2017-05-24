421f570e6c834e113dcdbcf460ceaa0416163bc0
tomas-polach <tomas@polach.ch>
2017-05-24 17:31:56 +0200

    Added destroy and remove methods

--- src/base3d/entity.js
+++ src/base3d/entity.js
@@ -1,15 +1,19 @@
+// utils
+import { generateUuid } from '../utils/uuid.js'
 // methods
+import add        from './entity/add.js'
+import destroy    from './entity/destroy.js'
 import find       from './entity/find.js'
 import findFirst  from './entity/find-first.js'
 import on         from './entity/on.js'
-import add        from './entity/add.js'
-// utils
-import { generateUuid } from '../utils/uuid.js'
+import remove     from './entity/remove.js'
+
 
 /**
  * @memeberof base3d
  * @class base3d.Entity
  */
+
 export default function Entity () {
   // Avoid direct this references (= less bugs and ES2015 compatible)
   var this_ = this
@@ -21,7 +25,11 @@ export default function Entity () {
 
 }
 
-Entity.prototype.on = on
+// methods
+
 Entity.prototype.add = add
+Entity.prototype.destroy = destroy
 Entity.prototype.find = find
 Entity.prototype.findFirst = findFirst
+Entity.prototype.on = on
+Entity.prototype.remove = remove
---
+++ src/base3d/entity/remove.js
@@ -0,0 +1,15 @@
+/**
+ * ...
+ * @memberof base3d
+ * @function Entity#remove
+ * @param   {object}                          args
+ * @param   {string}                          [args.apiUrl]           - Url of archilogic services server-side endpoints.
+ * @param   {string}                          [args.modelMakerApiUrl] - Url of model maker services server-side endpoints.
+ * @param   {('error'|'warn'|'info'|'debug')} [args.logLevel=warn]         - Specify logging level
+ * @returns {Promise}
+ */
+export default function remove () {
+
+  console.log('Remove')
+
+}
