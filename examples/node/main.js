19e3bfa20036e0bdf5ea8cf7583a9858b351062a
tomas-polach <tomas@polach.ch>
2017-05-24 17:31:14 +0200

    Updated example

--- examples/node/main.js
+++ examples/node/main.js
@@ -1,3 +1,5 @@
-var Base3d = require('../../build/base3d.js')
+var base3d = require('../../build/base3d.js')
 
-var b3d = new Base3d()
+var scene = new base3d.Entity()
+
+console.log('scene', scene)
