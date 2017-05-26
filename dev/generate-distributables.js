e0b7ba3d92a59cc82154467cca7481ff9c05033c
tomas-polach <tomas@polach.ch>
2017-05-26 11:10:10 +0200

    Use uglify directly and keep preamble

--- dev/generate-distributables.js
+++ dev/generate-distributables.js
@@ -1,89 +1,48 @@
-const Promise = require('bluebird')
-const spawn = require('child_process').spawn
+const run = require('./utils/run-cli-cmd.js')
+const fs = require('fs')
+const UglifyJS = require('uglify-js')
 const version = require('../package.json').version
 
+const distName = `base-query-${version}`
+
 run([
-  // empty build fodler
-  'rm -f build/*',
-  // bundle with rollup
-  'node_modules/rollup/bin/rollup -c',
+
+  // build
+  `npm run build`,
   // empty distribution folder
   'rm -f dist/*',
   // copy build files to distribution folder
-  `cp -R build/ dist/`,
+  `cp build/base-query.js dist/${distName}.js`,
+  `cp build/base-query.js.map dist/${distName}.js.map`,
   // uglify
-  'node_modules/uglify-js/bin/uglifyjs dist/base-query.js -o dist/base-query.min.js -cm --source-map includeSources,content=build/base-query.js.map,url=base-query.min.js.map',
+  () => {
+    const code = fs.readFileSync(`dist/${distName}.js`, `utf8`)
+    const map  = fs.readFileSync(`dist/${distName}.js.map`, `utf8`)
+    const ugly = UglifyJS.minify(code, {
+      warnings: true,
+      ie8: false,
+      compress: { dead_code: true, toplevel: true, passes: 3},
+      output: { preamble: extractPreamble(code) },
+      sourceMap: { content: map, url: `${distName}.min.js.map` }
+    })
+    fs.writeFileSync(`dist/${distName}.min.js`, ugly.code)
+    fs.writeFileSync(`dist/${distName}.min.js.map`, ugly.map)
+  },
   // gzip uglified files
-  `gzip -9 dist/base-query.min.js dist/base-query.min.js.map`,
-  // remove .gz extension and add version to name
-  `mv dist/base-query.min.js.gz dist/base-query-${version}.min.js`,
-  `mv dist/base-query.min.js.map.gz dist/base-query-${version}.min.js.map`,
-  // add version to name of non-uglified files
-  `mv dist/base-query.js dist/base-query-${version}.js`,
-  `mv dist/base-query.js.map dist/base-query-${version}.js.map`
+  `gzip -9 dist/${distName}.min.js dist/${distName}.min.js.map`,
+  // remove .gz extension
+  `mv dist/${distName}.min.js.gz dist/${distName}.min.js`,
+  `mv dist/${distName}.min.js.map.gz dist/${distName}.min.js.map`
+
 ]).catch((err) => {
   console.error(err)
 })
 
-/////////////////////////////////// helpers
 
-function run (a, b) {
+// helpers
 
-  if (typeof a === 'string') {
-    var lines = a.split('\n')
-    if (lines.length === 1) {
-      // is single command
-      return runSingleCmd(a, b)
-    } else {
-      // is multiple commands
-      a = lines
-    }
-  }
-
-  if (Array.isArray(a)) {
-    // run multiple commands
-    return Promise.mapSeries(a, (args) => {
-      if (Array.isArray(args)) {
-        return runSingleCmd(args[0], args[1])
-      } else if (typeof args === 'function') {
-        return args()
-      } else {
-        console.log(args)
-        return runSingleCmd(args)
-      }
-    })
-  }
-
+function extractPreamble(code) {
+  // extract preamble from code if present
+  var p = /(^\/\/.*$|\/\*.*\n(.*\n)*.*\*\/.*\n)/m.exec(code)
+  return p ? p[1] : null
 }
-
-function runSingleCmd (cmd, args) {
-  return new Promise((resolve, reject) => {
-
-    let ls, parts
-
-    if (args) {
-      ls = spawn(cmd, args)
-    } else {
-      parts = cmd.split(' ')
-      ls = spawn(parts[0], parts.slice(1))
-    }
-
-    ls.stdout.on('data', (data) => {
-      console.log(`${data}`)
-    })
-
-    ls.stderr.on('data', (data) => {
-      console.error(`${data}`)
-    })
-
-    ls.on('close', (code) => {
-      if (code === 0) {
-        resolve()
-      } else {
-        reject()
-      }
-    })
-
-  })
-}
-
