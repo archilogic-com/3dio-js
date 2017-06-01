d935ee62b10ed056519359a889f179988e7ca117
tomas-polach <tomas@polach.ch>
2017-06-01 13:51:55 +0200

    updates example

--- examples/node/main.js
+++ examples/node/main.js
@@ -6,37 +6,8 @@ var s3Prefix = 'https://dnvf9esa6v418.cloudfront.net'
 var url = s3Prefix + s3Key
 
 // send request
-console.time('fetch buffer')
-fetch(url)
-  .then(function(res) {
-    // get buffer
-    return res.buffer()
-  })
-  .then(function(buffer){
-    console.timeEnd('fetch buffer')
-
-    // convert to arrayBuffer
-    console.time('conver to arrayBuffer')
-    var arrayBuffer = convertToArrayBuffer(buffer)
-    console.timeEnd('conver to arrayBuffer')
-
-    // decode to data3d
-    console.time('decode data3d')
-    return BASE.data3d.decodeBuffer( arrayBuffer )
-  })
-  .then(function(data3d){
-    console.timeEnd('decode data3d')
-    // log
-
-  })
-
-
-// helper
-
-function convertToArrayBuffer (b) {
-  return b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength)
-}
-
-function convertToUint32Array (b) {
-  return new Uint32Array(b.buffer, b.byteOffset, b.byteLength / Uint32Array.BYTES_PER_ELEMENT)
-}
+fetch(url).then(function(res) {
+  return res.buffer()
+}).then(BASE.data3d.decodeBuffer).then(function(data3d){
+  console.log('Data3d has '+Object.keys(data3d.materials).length+' materials.')
+})
