3cb680b34062e7856af17a5bef8f56a1a6212acf
tomas-polach <tomas@polach.ch>
2017-06-01 16:23:59 +0200

    uses new module name and loads data3d

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -13,12 +13,14 @@
 //    console.log('Entity material data updated (loaded or updated by param change). uuid:'+geometry)
 //  })
 
-// create scene
-var scene = new bq.Entity()
-//console.log('Created scene (root level entity) with uuid:' + scene.uuid)
-// initiate loading
+var s3Key = '/3f995099-d624-4c8e-ab6b-1fd5e3799173/170420-1105-7yb890/8db476b3-7eb7-45a7-a09b-03e965f12685.gz.data3d.buffer'
 
-bq.add('../assets/cubes.base.json')
+var s3Prefix = 'https://dnvf9esa6v418.cloudfront.net'
+var url = s3Prefix + s3Key
 
-// TODO: renderer: catch error case of unknown uuid (exception: add)
-// TODO: bq: detect invalid hierarchies (ie. cycles) in file reference and dynamic scene handling
+// send request
+fetch(url).then(function(res) {
+  return res.buffer ? res.buffer() : res.arrayBuffer()
+}).then(BASE.data3d.decodeBuffer).then(function(data3d){
+  console.log('Data3d has '+Object.keys(data3d.materials).length+' materials.')
+})
