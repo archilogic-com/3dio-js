fe9a1fc31e69989246b531bacd56c43a7a738042
tomas-polach <tomas@polach.ch>
2017-06-01 16:31:23 +0200

    removes comments

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -1,18 +1,3 @@
-// pushing data to the renderer
-
-//  // hierarchy
-//  bq.on('**/*:add', function(uuid, entity){
-//    console.log('Added entity to scene. uuid:'+entity)
-//  })
-//  // geometry
-//  bq.on('**/*:data:geometry', function(uuid, entity, geometry){
-//    console.log('Entity geometry data updated (loaded or updated by param change). uuid:'+geometry)
-//  })
-//  // material
-//  bq.on('**/*:data:material', function(uuid, entity, material){
-//    console.log('Entity material data updated (loaded or updated by param change). uuid:'+geometry)
-//  })
-
 var s3Key = '/3f995099-d624-4c8e-ab6b-1fd5e3799173/170420-1105-7yb890/8db476b3-7eb7-45a7-a09b-03e965f12685.gz.data3d.buffer'
 
 var s3Prefix = 'https://dnvf9esa6v418.cloudfront.net'
