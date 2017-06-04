9874a0a0e70e7a974a2f2cc06e8fd3ff77ca6fb1
tomas-polach <tomas@polach.ch>
2017-06-05 01:47:34 +0200

    adds use case scenarios

--- examples/node/main.js
+++ examples/node/main.js
@@ -1,13 +1,18 @@
 var BASE = require('../../build/base-query.js')
 
-var key = '/3f995099-d624-4c8e-ab6b-1fd5e3799173/170420-1105-7yb890/8db476b3-7eb7-45a7-a09b-03e965f12685.gz.data3d.buffer'
+// var key = '/3f995099-d624-4c8e-ab6b-1fd5e3799173/170420-1105-7yb890/8db476b3-7eb7-45a7-a09b-03e965f12685.gz.data3d.buffer'
+var key = '/fd72bf39-9d3a-471f-a4ff-ecaa3f5ff30b/bake/2017-04-15_22-45-14_XsiltX/regular/lighting.gz.data3d.buffer'
 
-// // get object from base store
+
+// get object from base store
 // BASE.store.get(key).then(function(data3d){
-//
-//   console.log('Data3d has '+Object.keys(data3d.materials).length+' materials.')
-//
+//   console.log('materials: ', data3d.materials)
 // })
 
 
-BASE.product.get('d23062b7-6ec4-45c0-9cee-c96d835b5f54').then(console.log)
+// BASE.product.get('d23062b7-6ec4-45c0-9cee-c96d835b5f54').then(console.log)
+
+
+// BASE.store.put( Buffer.from('Hello World!', 'utf-8') ).then(function(key){
+//   console.log(key)
+// }).catch(console.error)
