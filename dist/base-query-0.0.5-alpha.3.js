c71c45669902c72436761fc05c61020e24fa8615
tomas-polach <tomas@polach.ch>
2017-06-04 22:51:10 +0200

    updates version number

--- dist/base-query-0.0.5-alpha.3.js
+++ dist/base-query-0.0.5-alpha.3.js
@@ -1,7 +1,7 @@
 /**
  * @preserve
  * @name base-query
- * @version 0.0.5-alpha.2
+ * @version 0.0.5-alpha.3
  * @description Parse and edit building data using simple APIs.
  * @see http://base-query.org/
  * @tutorial https://github.com/archilogic-com/base-query
@@ -5921,272 +5921,13 @@
 	}
 
 	var name = "base-query";
-	var version = "0.0.5-alpha.2";
+	var version = "0.0.5-alpha.3";
 
 	var homepage = "http://base-query.org/";
 
-	var logger = createCommonjsModule(function (module) {
-	/*!
-	 * js-logger - http://github.com/jonnyreeves/js-logger
-	 * Jonny Reeves, http://jonnyreeves.co.uk/
-	 * js-logger may be freely distributed under the MIT license.
-	 */
-	(function (global) {
-		"use strict";
-
-		// Top level module for the global, static logger instance.
-		var Logger = { };
-
-		// For those that are at home that are keeping score.
-		Logger.VERSION = "1.3.0";
-
-		// Function which handles all incoming log messages.
-		var logHandler;
-
-		// Map of ContextualLogger instances by name; used by Logger.get() to return the same named instance.
-		var contextualLoggersByNameMap = {};
-
-		// Polyfill for ES5's Function.bind.
-		var bind = function(scope, func) {
-			return function() {
-				return func.apply(scope, arguments);
-			};
-		};
-
-		// Super exciting object merger-matron 9000 adding another 100 bytes to your download.
-		var merge = function () {
-			var args = arguments, target = args[0], key, i;
-			for (i = 1; i < args.length; i++) {
-				for (key in args[i]) {
-					if (!(key in target) && args[i].hasOwnProperty(key)) {
-						target[key] = args[i][key];
-					}
-				}
-			}
-			return target;
-		};
-
-		// Helper to define a logging level object; helps with optimisation.
-		var defineLogLevel = function(value, name) {
-			return { value: value, name: name };
-		};
-
-		// Predefined logging levels.
-		Logger.DEBUG = defineLogLevel(1, 'DEBUG');
-		Logger.INFO = defineLogLevel(2, 'INFO');
-		Logger.TIME = defineLogLevel(3, 'TIME');
-		Logger.WARN = defineLogLevel(4, 'WARN');
-		Logger.ERROR = defineLogLevel(8, 'ERROR');
-		Logger.OFF = defineLogLevel(99, 'OFF');
-
-		// Inner class which performs the bulk of the work; ContextualLogger instances can be configured independently
-		// of each other.
-		var ContextualLogger = function(defaultContext) {
-			this.context = defaultContext;
-			this.setLevel(defaultContext.filterLevel);
-			this.log = this.info;  // Convenience alias.
-		};
-
-		ContextualLogger.prototype = {
-			// Changes the current logging level for the logging instance.
-			setLevel: function (newLevel) {
-				// Ensure the supplied Level object looks valid.
-				if (newLevel && "value" in newLevel) {
-					this.context.filterLevel = newLevel;
-				}
-			},
-
-			// Is the logger configured to output messages at the supplied level?
-			enabledFor: function (lvl) {
-				var filterLevel = this.context.filterLevel;
-				return lvl.value >= filterLevel.value;
-			},
-
-			debug: function () {
-				this.invoke(Logger.DEBUG, arguments);
-			},
-
-			info: function () {
-				this.invoke(Logger.INFO, arguments);
-			},
-
-			warn: function () {
-				this.invoke(Logger.WARN, arguments);
-			},
-
-			error: function () {
-				this.invoke(Logger.ERROR, arguments);
-			},
-
-			time: function (label) {
-				if (typeof label === 'string' && label.length > 0) {
-					this.invoke(Logger.TIME, [ label, 'start' ]);
-				}
-			},
-
-			timeEnd: function (label) {
-				if (typeof label === 'string' && label.length > 0) {
-					this.invoke(Logger.TIME, [ label, 'end' ]);
-				}
-			},
-
-			// Invokes the logger callback if it's not being filtered.
-			invoke: function (level, msgArgs) {
-				if (logHandler && this.enabledFor(level)) {
-					logHandler(msgArgs, merge({ level: level }, this.context));
-				}
-			}
-		};
-
-		// Protected instance which all calls to the to level `Logger` module will be routed through.
-		var globalLogger = new ContextualLogger({ filterLevel: Logger.OFF });
-
-		// Configure the global Logger instance.
-		(function() {
-			// Shortcut for optimisers.
-			var L = Logger;
-
-			L.enabledFor = bind(globalLogger, globalLogger.enabledFor);
-			L.debug = bind(globalLogger, globalLogger.debug);
-			L.time = bind(globalLogger, globalLogger.time);
-			L.timeEnd = bind(globalLogger, globalLogger.timeEnd);
-			L.info = bind(globalLogger, globalLogger.info);
-			L.warn = bind(globalLogger, globalLogger.warn);
-			L.error = bind(globalLogger, globalLogger.error);
-
-			// Don't forget the convenience alias!
-			L.log = L.info;
-		}());
-
-		// Set the global logging handler.  The supplied function should expect two arguments, the first being an arguments
-		// object with the supplied log messages and the second being a context object which contains a hash of stateful
-		// parameters which the logging function can consume.
-		Logger.setHandler = function (func) {
-			logHandler = func;
-		};
-
-		// Sets the global logging filter level which applies to *all* previously registered, and future Logger instances.
-		// (note that named loggers (retrieved via `Logger.get`) can be configured independently if required).
-		Logger.setLevel = function(level) {
-			// Set the globalLogger's level.
-			globalLogger.setLevel(level);
-
-			// Apply this level to all registered contextual loggers.
-			for (var key in contextualLoggersByNameMap) {
-				if (contextualLoggersByNameMap.hasOwnProperty(key)) {
-					contextualLoggersByNameMap[key].setLevel(level);
-				}
-			}
-		};
-
-		// Retrieve a ContextualLogger instance.  Note that named loggers automatically inherit the global logger's level,
-		// default context and log handler.
-		Logger.get = function (name) {
-			// All logger instances are cached so they can be configured ahead of use.
-			return contextualLoggersByNameMap[name] ||
-				(contextualLoggersByNameMap[name] = new ContextualLogger(merge({ name: name }, globalLogger.context)));
-		};
-
-		// CreateDefaultHandler returns a handler function which can be passed to `Logger.setHandler()` which will
-		// write to the window's console object (if present); the optional options object can be used to customise the
-		// formatter used to format each log message.
-		Logger.createDefaultHandler = function (options) {
-			options = options || {};
-
-			options.formatter = options.formatter || function defaultMessageFormatter(messages, context) {
-				// Prepend the logger's name to the log message for easy identification.
-				if (context.name) {
-					messages.unshift("[" + context.name + "]");
-				}
-			};
-
-			// Map of timestamps by timer labels used to track `#time` and `#timeEnd()` invocations in environments
-			// that don't offer a native console method.
-			var timerStartTimeByLabelMap = {};
-
-			// Support for IE8+ (and other, slightly more sane environments)
-			var invokeConsoleMethod = function (hdlr, messages) {
-				Function.prototype.apply.call(hdlr, console, messages);
-			};
-
-			// Check for the presence of a logger.
-			if (typeof console === "undefined") {
-				return function () { /* no console */ };
-			}
-
-			return function(messages, context) {
-				// Convert arguments object to Array.
-				messages = Array.prototype.slice.call(messages);
-
-				var hdlr = console.log;
-				var timerLabel;
-
-				if (context.level === Logger.TIME) {
-					timerLabel = (context.name ? '[' + context.name + '] ' : '') + messages[0];
-
-					if (messages[1] === 'start') {
-						if (console.time) {
-							console.time(timerLabel);
-						}
-						else {
-							timerStartTimeByLabelMap[timerLabel] = new Date().getTime();
-						}
-					}
-					else {
-						if (console.timeEnd) {
-							console.timeEnd(timerLabel);
-						}
-						else {
-							invokeConsoleMethod(hdlr, [ timerLabel + ': ' +
-								(new Date().getTime() - timerStartTimeByLabelMap[timerLabel]) + 'ms' ]);
-						}
-					}
-				}
-				else {
-					// Delegate through to custom warn/error loggers if present on the console.
-					if (context.level === Logger.WARN && console.warn) {
-						hdlr = console.warn;
-					} else if (context.level === Logger.ERROR && console.error) {
-						hdlr = console.error;
-					} else if (context.level === Logger.INFO && console.info) {
-						hdlr = console.info;
-					}
-
-					options.formatter(messages, context);
-					invokeConsoleMethod(hdlr, messages);
-				}
-			};
-		};
-
-		// Configure and example a Default implementation which writes to the `window.console` (if present).  The
-		// `options` hash can be used to configure the default logLevel and provide a custom message formatter.
-		Logger.useDefaults = function(options) {
-			Logger.setLevel(options && options.defaultLevel || Logger.DEBUG);
-			Logger.setHandler(Logger.createDefaultHandler(options));
-		};
-
-		// Export to popular environments boilerplate.
-		if (typeof undefined === 'function' && undefined.amd) {
-			undefined(Logger);
-		}
-		else if ('object' !== 'undefined' && module.exports) {
-			module.exports = Logger;
-		}
-		else {
-			Logger._prevLogger = global.Logger;
-
-			Logger.noConflict = function () {
-				global.Logger = Logger._prevLogger;
-				return Logger;
-			};
-
-			global.Logger = Logger;
-		}
-	}(commonjsGlobal));
-	});
-
+	//import Logger from 'js-logger'
 	// Bootstrap logger
-	logger.useDefaults();
+	//Logger.useDefaults()
 
 	// print header to console in browser environment
 
@@ -13855,6 +13596,111 @@
 	  return url
 	}
 
+	var FormData_;
+	if (runtime.isNode) {
+	  FormData_ = require('form-data');
+	} else if (typeof FormData !== 'undefined') {
+	  FormData_ = FormData;
+	} else {
+	  console.warn('Missing FormData API.');
+	  FormData_ = function FormDataError () {
+	    throw new Error('Missing FormData API.')
+	  };
+	}
+
+	var FormData$1 = FormData_;
+
+	var FALLBACK_MIME_TYPE = 'application/octet-stream';
+	var EXTENSION_TO_MIME_TYPE = {
+	    obj: 'text/plain',
+	    dds: 'application/octet-stream',
+	    dwg: 'application/acad',
+	    dxf: 'application/dxf',
+	    jpg: 'image/jpeg',
+	    jpeg: 'image/jpeg',
+	    png: 'image/png',
+	    gif: 'image/gif',
+	    txt: 'text/plain',
+	    log: 'text/plain',
+	    svg: 'svg+xml',
+	    html: 'text/html',
+	    htm: 'text/html',
+	    js: 'application/javascript',
+	    json: 'application/json',
+	    md: 'text/markdown',
+	    csv: 'text/csv',
+	    gz:	'application/x-gzip',
+	    gzip:	'application/x-gzip',
+	    zip:'application/x-zip',
+	    pdf: 'application/pdf',
+	    '3ds': 'application/x-3ds'
+	  };
+
+	function getMimeTypeFromFileName (filename) {
+	  var
+	    result = FALLBACK_MIME_TYPE,
+	    extension;
+
+	  // get extension if file has one
+	  if (filename.indexOf('.') > -1) {
+	    extension = filename.split('.').pop().toLowerCase();
+	    if (EXTENSION_TO_MIME_TYPE[extension]) {
+	      // set mime type if it exists in the map
+	      result = EXTENSION_TO_MIME_TYPE[extension];
+	    }
+	  }
+
+	  return result
+	}
+
+	// constants
+
+	var KEY_PATTERN = /<Key>(.*)<\/Key>/gi;
+
+	// main
+
+	function putToStore (file, options) {
+
+	  options = options || {};
+	  var fileName = file.name || options.fileName || 'unnamed.txt';
+
+	  // get credentials for S3
+	  return callService('S3.getCredentials', {
+	    contentLength: file.size || file.length,
+	    contentType: getMimeTypeFromFileName(fileName),
+	    fileName: fileName
+	  }).then(function (credentials) {
+
+	    // upload directly to S3 using credentials
+	    var fd = new FormData$1();
+	    fd.append('key', credentials.key);
+	    fd.append('AWSAccessKeyId', credentials.AWSAccessKeyId);
+	    fd.append('acl', credentials.acl);
+	    fd.append('Content-Type', credentials.contentType);
+	    fd.append('policy', credentials.policy);
+	    fd.append('signature', credentials.signature);
+	    fd.append('success_action_status', '201');
+	    if (credentials.contentEncoding) {
+	      fd.append('Content-Encoding', credentials.contentEncoding);
+	    }
+	    //fd.append('success_action_redirect', 'http://google.com')
+	    //fd.append('expiration',s3Credetials.plainPolicy.expiration)
+	    fd.append('file', file);
+
+	    // upload to S3
+	    return fetch$1(credentials.url, {method: 'POST', body: fd})
+	      .then(function (res) {
+	        return res.text()
+	      }).then(function (s3Response) {
+	        // get file key from response
+	        var s = KEY_PATTERN.exec(s3Response);
+	        return s ? '/'+s[1] : Promise.reject('Error Uploading File: '+s3Response)
+	      })
+
+	  })
+
+	}
+
 	function getProduct (id) {
 	  // FIMXE: use proper argument name
 	  return callService('Product.read', { id:id }).then(function(productInfo){
@@ -13884,7 +13730,8 @@
 	    decodeBuffer: decodeBuffer,
 	  },
 	  store: {
-	    get: getFromStore
+	    get: getFromStore,
+	    put: putToStore
 	  },
 	  product: {
 	    get: getProduct
@@ -13892,7 +13739,9 @@
 	  services: {
 	    call: callService
 	  },
-	  // ui: {},
+	  ui: {
+	    getFilesFromDragAndDropEvent: loadData3d
+	  },
 	  // scene: {},
 	  // space: {},
 	  // channel: { subscribe:… },
