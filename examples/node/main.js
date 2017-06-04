7c1dbfca1e951c21c0efd44c84b49082ad1ac781
tomas-polach <tomas@polach.ch>
2017-06-04 10:44:28 +0200

    updates examples

--- examples/node/main.js
+++ examples/node/main.js
@@ -2,9 +2,12 @@ var BASE = require('../../build/base-query.js')
 
 var key = '/3f995099-d624-4c8e-ab6b-1fd5e3799173/170420-1105-7yb890/8db476b3-7eb7-45a7-a09b-03e965f12685.gz.data3d.buffer'
 
-// get object from base store
-BASE.store.get(key).then(function(data3d){
+// // get object from base store
+// BASE.store.get(key).then(function(data3d){
+//
+//   console.log('Data3d has '+Object.keys(data3d.materials).length+' materials.')
+//
+// })
 
-  console.log('Data3d has '+Object.keys(data3d.materials).length+' materials.')
 
-})
+BASE.product.get('d23062b7-6ec4-45c0-9cee-c96d835b5f54').then(console.log)
