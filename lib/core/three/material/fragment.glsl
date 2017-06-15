d9d4eaec4877d2c26a0111309e3837000192415f
madlaina-kalunder <kalunder@archilogic.com>
2017-06-15 16:02:35 +0200

    Added map includes

--- lib/core/three/material/fragment.glsl
+++ lib/core/three/material/fragment.glsl
@@ -1,4 +1,5 @@
-uniform sampler2D map;
+#include <map_pars_fragment>
+
 uniform sampler2D lightMap;
 uniform vec3 color;
 uniform vec3 specular;
@@ -8,14 +9,27 @@ uniform float opacity;
 #include <uv2_pars_fragment>
 
 void main() {
-  #if defined( USE_LIGHTMAP )
-    gl_FragColor = texture2D(lightMap, vUv2);
-  #else
-    gl_FragColor = vec4(color, opacity);
-  #endif
+  //vec3 diffuse = vec3(0.5, 0.5, 0.9);
+  vec3 diffuse = vec3(1.0, 1.0, 1.0);
+  vec4 diffuseColor = vec4( diffuse, opacity );
 
-  #if defined( USE_MAP )
-    vec3 mapColor = texture2D(map, vUv, 0.0).rgb;
-    gl_FragColor *= vec4(mapColor, 1.0) * 1.1;
-  #endif
+
+  #include <map_fragment>
+
+ gl_FragColor = diffuseColor;
+
+  /* ////////// custom
+   #if defined( USE_LIGHTMAP )
+     gl_FragColor = texture2D(lightMap, vUv2);
+   #else
+     gl_FragColor = vec4(color, opacity);
+   #endif
+
+   #if defined( USE_MAP )
+     vec3 mapColor = texture2D(map, vUv, 0.0).rgb;
+     gl_FragColor *= vec4(mapColor, 1.0) * 1.1;
+   #endif
+
+  */
+
 }
