afdda9af0b3314624dbadc5a4e8ac81d10fb7121
madlaina-kalunder <kalunder@archilogic.com>
2017-06-16 18:00:53 +0200

    Added relevant includes from THREE phong material.

--- lib/core/three/material/base-fragment.glsl
+++ lib/core/three/material/base-fragment.glsl
@@ -1,22 +1,54 @@
-#include <map_pars_fragment>
-
-uniform sampler2D lightMap;
-uniform vec3 color;
-uniform vec3 specular;
+uniform vec3 diffuse; //-> diffuse color
+uniform vec3 emissive;
+uniform vec3 specular; // -> specular color
+uniform float shininess;
 uniform float opacity;
 
+#include <common>
 #include <uv_pars_fragment>
 #include <uv2_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <lightmap_pars_fragment>
+#include <normalmap_pars_fragment>
+#include <specularmap_pars_fragment>
+
+#include <bsdfs> //??
+#include <lights_pars>
+#include <lights_phong_pars_fragment>
+
 
 void main() {
-  //vec3 diffuse = vec3(0.5, 0.5, 0.9);
-  vec3 diffuse = vec3(1.0, 1.0, 1.0);
+
   vec4 diffuseColor = vec4( diffuse, opacity );
+  ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
 
+  vec3 totalEmissiveRadiance = emissive;
 
+  #include <logdepthbuf_fragment>
   #include <map_fragment>
+  #include <alphamap_fragment>
+  #include <alphatest_fragment>
+	#include <specularmap_fragment>
+	#include <normal_flip>
+  #include <normal_fragment>
 
- gl_FragColor = diffuseColor;
+  // accumulation
+  #include <lights_phong_fragment>
+  #include <lights_template>
+
+  // modulation
+  //#include <lightmap_fragment>
+  //#include <aomap_fragment>
+
+  vec3 outgoingLight = vec3(specularStrength);//reflectedLight.directDiffuse;// + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+
+  gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+
+  /*#if defined( USE_NORMALMAP )
+    vec3 mapColor = texture2D(normalMap, vUv, 0.0).rgb;
+    gl_FragColor = vec4(mapColor, 1.0) * 1.1;
+  #endif*/
 
   /* ////////// custom
    #if defined( USE_LIGHTMAP )
--- lib/core/three/material/base-vertex.glsl
+++ lib/core/three/material/base-vertex.glsl
@@ -1,3 +1,9 @@
+varying vec3 vViewPosition;
+
+#ifndef FLAT_SHADED
+	varying vec3 vNormal;
+#endif
+
 #include <uv_pars_vertex>
 #include <uv2_pars_vertex>
 
@@ -7,6 +13,20 @@ void main()
   #include <uv_vertex>
   #include <uv2_vertex>
 
+  #include <beginnormal_vertex> //vec3 objectNormal = vec3( normal );
+  #include <defaultnormal_vertex> // Flip side + vec3 transformedNormal = normalMatrix * objectNormal;
+
+  #ifndef FLAT_SHADED // Normal computed with derivatives when FLAT_SHADED
+  	vNormal = normalize( transformedNormal );
+  #endif
+
+  #include <begin_vertex> // vec3 transformed = vec3( position );
+  #include <project_vertex> //vec4 mvPosition = modelViewMatrix * vec4( transformed, 1.0 ); set gl_position
+
+  vViewPosition = - mvPosition.xyz;
+
+
+  /*////////// custom
   vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-  gl_Position = projectionMatrix * mvPosition;
+  gl_Position = projectionMatrix * mvPosition;*/
 }
