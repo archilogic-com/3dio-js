385415362ac03ebdbdf010965535e7339f4563f8
tomas-polach <tomas@polach.ch>
2017-07-17 12:36:46 +0100

    Fixed references

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
-import decodeBuffer                 from './core/data3d/decode-buffer.js'
-import loadData3d                   from './core/data3d/load.js'
-import request                      from './core/io/request.js'
-import fetch                        from './core/io/fetch.js'
-import getMimeTypeFromFilename      from './core/utils/get-mime-type-from-filename.js'
-import uuid                         from './core/utils/uuid.js'
-import url                          from './core/utils/url.js'
-import path                         from './core/utils/path.js'
-import wait                         from './core/utils/wait.js'
-import callService                  from './core/services/call.js'
+import decodeBuffer                 from './utils/data3d/decode-buffer.js'
+import loadData3d                   from './utils/data3d/load.js'
+import request                      from './utils/io/request.js'
+import fetch                        from './utils/io/fetch.js'
+import getMimeTypeFromFilename      from './utils/get-mime-type-from-filename.js'
+import uuid                         from './utils/uuid.js'
+import url                          from './utils/url.js'
+import path                         from './utils/path.js'
+import wait                         from './utils/wait.js'
+import callService                  from './utils/services/call.js'
 
 var utils = {
 
