12b005045baaf8b3fbf5bbccecfb33464d796714
tomas-polach <tomas@polach.ch>
2017-07-21 14:26:26 +0100

    Added constants for defaults

--- src/aframe/three/material/base-material.js
+++ src/aframe/three/material/base-material.js
@@ -2,6 +2,12 @@ import checkDependencies from '../../check-dependencies.js'
 import fragmentShader from './base-fragment.glsl'
 import vertexShader from './base-vertex.glsl'
 
+// CONFIGS
+
+var DEFAULT_LIGHT_MAP_INTENSITY = 1
+var DEFAULT_LIGHT_MAP_CENTER = 0.5
+var DEFAULT_LIGHT_MAP_FALLOFF = 0
+
 export default checkDependencies ({
   three: true,
   aframe: false
@@ -11,8 +17,8 @@ export default checkDependencies ({
     THREE.ShaderMaterial.call( this, params )
 
     var params = params || {}
-    this.lightMapCenter = params.lightMapCenter || 0.5
-    this.lightMapFalloff = params.lightMapFalloff || 0.5
+    this.lightMapCenter = params.lightMapCenter || DEFAULT_LIGHT_MAP_CENTER
+    this.lightMapFalloff = params.lightMapFalloff || DEFAULT_LIGHT_MAP_FALLOFF
 
     this.uniforms = THREE.UniformsUtils.merge( [
       THREE.UniformsLib[ "lights" ],
@@ -22,9 +28,9 @@ export default checkDependencies ({
         specularMap: { value: params.specularMap || null },
         alphaMap: { value: params.alphaMap || null },
         lightMap: { value: params.lightMap || null },
-        lightMapIntensity: { value: params.lightMapIntensity || 1.0 },
-        lightMapFalloff: { value: params.lightMapFalloff || 0.5 },
-        lightMapCenter: { value: params.lightMapCenter || 0.5 },
+        lightMapIntensity: { value: params.lightMapIntensity || DEFAULT_LIGHT_MAP_INTENSITY },
+        lightMapFalloff: { value: params.lightMapFalloff || DEFAULT_LIGHT_MAP_FALLOFF },
+        lightMapCenter: { value: params.lightMapCenter || DEFAULT_LIGHT_MAP_CENTER },
         normalMap: { value: params.normalMap || null },
         shininess: { value: params.shininess || 1.0 },
         specular: { value: params.specular || new THREE.Color(0.25, 0.25, 0.25) },
