14cd5636b6f038be7b3c47869a39dac9a685dfa2
Martin Splitt <mr.avgp@googlemail.com>
2017-08-08 13:14:52 +0200

    Use arrayBuffer

--- src/utils/data3d/load.js
+++ src/utils/data3d/load.js
@@ -3,7 +3,7 @@ import decodeBuffer from './decode-buffer.js'
 
 export default function loadData3d (url, options) {
   return fetch(url, options).then(function(res){
-    return res.buffer()
+    return res.arrayBuffer()
   }).then(function(buffer){
     return decodeBuffer(buffer, { url: url })
   })
