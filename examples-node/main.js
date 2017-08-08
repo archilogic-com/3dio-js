79b3393ccf8de7592a7d71f7c9eef6ce267e1e4c
tomas-polach <tomas@polach.ch>
2017-08-09 00:25:32 +0200

    fixed upper case and uncommented example code

--- examples-node/main.js
+++ examples-node/main.js
@@ -1,13 +1,13 @@
-var IO3D = require('../build/3dio.js')
+var io3d = require('../build/3dio.js')
 
 // var key = '/3f995099-d624-4c8e-ab6b-1fd5e3799173/170420-1105-7yb890/8db476b3-7eb7-45a7-a09b-03e965f12685.gz.data3d.buffer'
 var key = '/fd72bf39-9d3a-471f-a4ff-ecaa3f5ff30b/bake/2017-04-15_22-45-14_XsiltX/regular/lighting.gz.data3d.buffer'
 
 
 // get object from base store
-// io3d.storage.get(key).then(function(data3d){
-//   console.log('materials: ', data3d.materials)
-// })
+ io3d.storage.get(key).then(function(data3d){
+   console.log('materials: ', data3d.materials)
+ })
 
 
 // io3d.furniture.get('d23062b7-6ec4-45c0-9cee-c96d835b5f54').then(console.log)
