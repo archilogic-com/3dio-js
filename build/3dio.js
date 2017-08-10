af1caf8b67312a20ea281d308d804aa503cafc90
tomas-polach <tomas@polach.ch>
2017-08-10 22:12:23 +0200

    Release 1.0.0-beta.40

--- build/3dio.js
+++ build/3dio.js
@@ -1,10 +1,10 @@
 /**
  * @preserve
  * @name 3dio
- * @version 1.0.0-beta.39
- * @date 2017/08/10 22:08
+ * @version 1.0.0-beta.40
+ * @date 2017/08/10 22:12
  * @branch master
- * @commit 034bd01442fd74614bb05e8689e7431e6fd336ef
+ * @commit 525ff9d1037379a72a39211b775f3c226938076f
  * @description toolkit for interior apps
  * @see https://3d.io
  * @tutorial https://github.com/archilogic-com/3dio-js
@@ -18,7 +18,7 @@
 	(global.io3d = factory());
 }(this, (function () { 'use strict';
 
-	var BUILD_DATE='2017/08/10 22:08', GIT_BRANCH = 'master', GIT_COMMIT = '034bd01442fd74614bb05e8689e7431e6fd336ef'
+	var BUILD_DATE='2017/08/10 22:12', GIT_BRANCH = 'master', GIT_COMMIT = '525ff9d1037379a72a39211b775f3c226938076f'
 
 	/**
 	 * @license RequireJS domReady 2.0.1 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
@@ -7318,7 +7318,7 @@
 	  })();
 	}
 
-	var version = "1.0.0-beta.39";
+	var version = "1.0.0-beta.40";
 
 
 	var homepage = "https://3d.io";
