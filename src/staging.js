10e310d020ac2e262e8e2a9d6a35b56eeb99d04d
frederic-schwarz <schwarz@archilogic.com>
2017-09-08 16:12:54 +0200

    Removed place-furnishings #41

--- src/staging.js
+++ src/staging.js
@@ -1,10 +1,8 @@
 import getFurnishings from './staging/get-furnishings.js'
-import placeFurnishings from './staging/place-furnishings.js'
 import replaceFurniture from './staging/replace-furniture.js'
 
 var staging = {
   getFurnishings: getFurnishings,
-  placeFurnishings: placeFurnishings,
   replaceFurniture: replaceFurniture
 }
 
