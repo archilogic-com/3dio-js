bc0ab40d117a1b64eb641534281c2b2dde39a751
Benjamin Devine <benjamin.devine@gmail.com>
2017-09-13 23:17:46 +0200

    Allowed for username to be passed to auth method

--- src/utils/auth/log-in.js
+++ src/utils/auth/log-in.js
@@ -7,49 +7,52 @@ import getSession from './get-session.js'
  * Login in user using credentials
  * @function io3d.auth.logIn
  * @param {object} args
- * @param {string} args.email - User email or username
+ * @param {string} args.username - User username or email
  * @param {string} args.password - User password
  */
 
-export default function logIn (args) {
-
+export default function logIn(args) {
   var credentials = {
-    email: args.email,
+    username: args.username || args.email,
     password: args.password || uuid.generate()
   }
 
   // log out first
-  log.debug('Sending API login request for user "' + credentials.email + '" ...')
-  return callServices('User.logOut').then(function onLogoutSuccess() {
-
-    // send log in request
-    return callServices('User.logIn', {
-      loginData: {
-        resourceName: credentials.email,
-        password: credentials.password
+  log.debug(
+    'Sending API login request for user "' + credentials.username + '" ...'
+  )
+  return callServices('User.logOut')
+    .then(function onLogoutSuccess() {
+      // send log in request
+      return callServices('User.logIn', {
+        loginData: {
+          resourceName: credentials.username,
+          password: credentials.password
+        }
+      })
+    })
+    .then(function onLoginSuccess() {
+      // request session to verify login with a separate request
+      return getSession()
+    })
+    .then(function onSessionSuccess(session) {
+      if (session.isAuthenticated) {
+        log.debug(
+          'API: User "' + session.user.email + '" logged in successfully.'
+        )
+        return session
+      } else {
+        return Promise.reject(
+          'Log in error: Session could not been established.'
+        )
       }
     })
-
-  }).then(function onLoginSuccess () {
-
-    // request session to verify login with a separate request
-    return getSession()
-
-  }).then(function onSessionSuccess (session) {
-
-    if (session.isAuthenticated) {
-      log.debug('API: User "' + session.user.email + '" logged in successfully.')
-      return session
-    } else {
-      return Promise.reject('Log in error: Session could not been established.')
-    }
-
-  }).catch(function onError (error) {
-
-    // login failed
-    log.debug('API: Could not log in user "' + credentials.email + '".', error)
-    return Promise.reject(error)
-
-  })
-
+    .catch(function onError(error) {
+      // login failed
+      log.debug(
+        'API: Could not log in user "' + credentials.email + '".',
+        error
+      )
+      return Promise.reject(error)
+    })
 }
