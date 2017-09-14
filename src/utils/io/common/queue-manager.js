14d2989f730a8c0de8b7fbb660a7e55ee7fac95a
tomas-polach <tomas@polach.ch>
2017-09-14 16:00:05 +0300

    removes outcommented code and debuging functions

--- src/utils/io/common/queue-manager.js
+++ src/utils/io/common/queue-manager.js
@@ -39,103 +39,6 @@ for (var i = 0, l = _queuesLength; i < l; i++) {
   _concurrentPerQueue[queueName] = 0
 }
 
-/*
-
-(2017/09/13) Performance monitoring code
-
-loadingQueueGraph         = false;
-if (loadingQueueGraph)
-  window.loadingPerformanceHistory = new PerformanceGraph.PerformanceHistory(64);
-
-window.loadingQueueShowInfo = function() {
-  function padRight(text, fieldLength) {
-    return text + Array(fieldLength - text.length + 1).join(' ');
-  }
-  function padLeft(text, fieldLength) {
-    return Array(fieldLength - text.length + 1).join(' ') + text;
- }
- var text = '';
- var keys = Object.keys(queueInfo);
- var baseTime;
- if (keys.length > 0) {
-   var baseKey   = keys[0];
-   var baseValue = queueInfo[baseKey];
-   baseTime = baseValue.timeFirst;
-   text += '(' + baseTime.toFixed(1) + ')\n'
- }
- for (var i = 0; i < keys.length; i++) {
-   var key   = keys[i];
-   var value = queueInfo[key];
-   text += padRight(keys[i], 28) + ': ';
-   if (value.requestCount > 0) {
-     var t1 = value.timeFirst - baseTime;
-     var t2 = value.timeLast - baseTime;
-     var t1Str = t1.toFixed(1);
-     var t2Str = t2.toFixed(1);
-     var t3, d, t3Str, dStr;
-     if (value.timeLastFinished) {
-       t3 = value.timeLastFinished - baseTime;
-       d  = t3 - t1;
-       t3Str = t3.toFixed(1);
-       dStr  = d.toFixed(1);
-     } else {
-       t3Str = '-';
-       dStr  = '-';
-     }
-     text += padLeft(t1Str, 5) + ' - ' + padLeft(t2Str, 5) + ' / ' + padLeft(t3Str, 5) + ' (' + padLeft(dStr, 5) + ') [' + padLeft(value.requestCount.toString(), 4) + ']\n';
-   } else
-     text += '<inactive>\n'
- }
- console.log(text);
-}
-
-window.loadingQueueProcessData = function() {
- var loadingQueueDataLines = window.loadingQueueData.split('\n');
- var i = 0;
- while (i < loadingQueueDataLines.length) {
-   var line = loadingQueueDataLines[i];
-   var count = 0;
-   for (var j = i; i < loadingQueueDataLines.length; j++) {
-     var nextLine = loadingQueueDataLines[j];
-     if (line === nextLine)
-       count++;
-     else
-       break;
-   }
-   if (count > 8) {
-     loadingQueueDataLines.splice(i + 4, count - 8, ' ');
-     loadingQueueDataLines.
-       i += 4
-   } else
-     i += 1;
- }
- window.loadingQueueDataShort = loadingQueueDataLines.join('\n');
-}
-
-function _addPerformanceEntry() {
-  var performanceEntry = window.loadingPerformanceHistory.newEntry(PerformanceGraph.PerformanceEntry);
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    performanceEntry[queueName] = _concurrentPerQueue[queueName];
-  }
-  // performanceEntry.none = loadingQueuePipelineDepth - _concurrentRequests;
-}
-
-function _appendLoadingQueueData() {
-  var entry = '';
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    entry += _concurrentPerQueue[queueName].toString();
-    if (i < _queuesLength - 1)
-      entry += ', ';
-    else
-      entry += '\n';
-  }
-  window.loadingQueueData += entry;
-}
-
-*/
-
 function _startRequest(queueName) {
   // Update queue tracking information
   var info = queueInfo[queueName];
@@ -158,64 +61,7 @@ function _startRequest(queueName) {
   request.start();
 }
 
-function _doProcessQueueOriginal() {
-  if (_concurrentRequests >= loadingQueuePipelineDepth)
-    return;
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    if (_queues[queueName].length > 0) {
-      _startRequest(queueName);
-      break;
-    } else if (_concurrentPerQueue[queueName] !== 0)
-      break;
-  }
-}
-
-function _doProcessQueueOriginalFixed(){
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    while (_queues[queueName].length > 0 && _concurrentRequests < loadingQueuePipelineDepth)
-      _startRequest(queueName)
-    if (_concurrentPerQueue[queueName] !== 0)
-      break;
-  }
-}
-
-function _doProcessQueueOverstep(){
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    while (_queues[queueName].length > 0 && _concurrentRequests < loadingQueuePipelineDepth)
-      _startRequest(queueName)
-  }
-}
-
-function _doProcessQueueOverstepOne(){
-  var anchorStage = null;
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    while (_queues[queueName].length > 0 && _concurrentRequests < loadingQueuePipelineDepth)
-      _startRequest(queueName)
-    if (anchorStage === null && _concurrentPerQueue[queueName] !== 0)
-      anchorStage = i;
-    if (anchorStage !== null && i - anchorStage > 0)
-      break;
-  }
-}
-
-function _doProcessQueueOverstepFenced(){
-  var anchorStage = null;
-  for (var i = 0; i < _queuesLength; i++) {
-    var queueName = queuesByPriority[i];
-    while (_queues[queueName].length > 0 && _concurrentRequests < loadingQueuePipelineDepth)
-      _startRequest(queueName)
-    if (anchorStage === null && _concurrentPerQueue[queueName] !== 0)
-      anchorStage = i;
-    if (anchorStage !== null && _queueFences[queueName])
-      break;
-  }
-}
-
-function _doProcessQueueOverstepOneFenced(){
+function _processQueue() {
   var anchorStage = null;
   for (var i = 0; i < _queuesLength; i++) {
     var queueName = queuesByPriority[i];
@@ -228,35 +74,7 @@ function _doProcessQueueOverstepOneFenced(){
   }
 }
 
-function _processQueue() {
-  if (loadingQueueAlgorithm === 'original')
-    _doProcessQueueOriginal()
-  else if (loadingQueueAlgorithm === 'original-fixed')
-    _doProcessQueueOriginalFixed()
-  else if (loadingQueueAlgorithm === 'overstep')
-    _doProcessQueueOverstep()
-  else if (loadingQueueAlgorithm === 'overstep-one')
-    _doProcessQueueOverstepOne()
-  else if (loadingQueueAlgorithm === 'overstep-fenced')
-    _doProcessQueueOverstepFenced()
-  else if (loadingQueueAlgorithm === 'overstep-one-fenced')
-    _doProcessQueueOverstepOneFenced()
-  else
-    throw 'Http._processQueue: Unknown loading queue processing algorithm.'
-  /* (2017/09/13) Performance monitoring code
-  if (_queuesChanged) {
-    if (loadingQueueGraph)
-      _addPerformanceEntry();
-    _appendLoadingQueueData();
-    _queuesChanged = false;
-  }
-  */
-}
-
 function _enqueue(queueName, url){
-
-  //// fallback to last queue
-  //if (!_queues[queueName]) queueName = queuesByPriority[_queuesLength-1]
 
   // fallback to first queue
   if (!_queues[queueName]) {
