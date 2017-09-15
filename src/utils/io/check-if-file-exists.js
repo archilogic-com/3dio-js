3349600067da21720b27f3eb52a8270cb568b761
tomas-polach <tomas@polach.ch>
2017-09-15 18:36:03 +0300

    adds param check

--- src/utils/io/check-if-file-exists.js
+++ src/utils/io/check-if-file-exists.js
@@ -2,17 +2,17 @@ import fetch from './fetch.js'
 import addCacheBustToQuery from './common/add-cache-bust-to-query.js'
 
 export default function checkIfFileExists (url) {
-  return fetch(
-    addCacheBustToQuery(url),
-    {
-      method: 'HEAD',
-      cache: 'reload'
-    }
-  ).then(function onSuccess(response){
+
+  if (!url) return Promise.reject('Please provide file URL string as param.')
+
+  return fetch( addCacheBustToQuery(url), {
+    method: 'HEAD',
+    cache: 'reload'
+  }).then(function onSuccess (response) {
     // response.ok provides boolean type information on request status
     // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
     return response.ok
-  }, function onReject(){
+  }, function onReject () {
     return false
   })
 }
