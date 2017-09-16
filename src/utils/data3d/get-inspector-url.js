81c0746c2c745126293d91d054070b7fce9a2ffc
tomas-polach <tomas@polach.ch>
2017-09-16 11:48:29 +0300

    adds method: io3d.utils.data3d.getInspectorUrl(storageId)

---
+++ src/utils/data3d/get-inspector-url.js
@@ -0,0 +1,4 @@
+export default function getData3dInspectorUrl (storageId) {
+  // TODO: replace spaces SDK with mini app to inspect data3d files
+  return 'https://spaces.archilogic.com/3d/?mode=sdk&file='+storageId
+}
