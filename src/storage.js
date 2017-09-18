b7b7262a645fd509c4a1dde1ff87526f93b6d526
tomas-polach <tomas@polach.ch>
2017-09-18 19:11:44 +0200

    Adds io3d.storage.getNoCdnUrlFromStorageId() method

--- src/storage.js
+++ src/storage.js
@@ -1,12 +1,14 @@
 import put from './storage/put.js'
 import get from './storage/get.js'
 import getUrlFromStorageId from './storage/get-url-from-id.js'
+import getNoCdnUrlFromStorageId from './storage/get-no-cdn-url-from-id.js'
 import getStorageIdFromUrl from './storage/get-id-from-url.js'
 
 var storage = {
   get: get,
   put: put,
   getUrlFromStorageId: getUrlFromStorageId,
+  getNoCdnUrlFromStorageId: getNoCdnUrlFromStorageId,
   getStorageIdFromUrl: getStorageIdFromUrl
 }
 
