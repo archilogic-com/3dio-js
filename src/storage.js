f41e3173e7501e9ca19de19d1db3e9e1fab209e3
Benjamin Devine <benjamin.devine@gmail.com>
2017-09-27 13:53:06 +0200

    added exportObj, exportDae methods

--- src/storage.js
+++ src/storage.js
@@ -3,13 +3,18 @@ import get from './storage/get.js'
 import getUrlFromStorageId from './storage/get-url-from-id.js'
 import getNoCdnUrlFromStorageId from './storage/get-no-cdn-url-from-id.js'
 import getStorageIdFromUrl from './storage/get-id-from-url.js'
+import convert from './storage/convert.js'
+
+var { exportObj, exportDae } = convert
 
 var storage = {
   get: get,
   put: put,
   getUrlFromStorageId: getUrlFromStorageId,
   getNoCdnUrlFromStorageId: getNoCdnUrlFromStorageId,
-  getStorageIdFromUrl: getStorageIdFromUrl
+  getStorageIdFromUrl: getStorageIdFromUrl,
+  exportObj: exportObj,
+  exportDae: exportDae
 }
 
 export default storage
