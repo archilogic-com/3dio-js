e76885fa181a87fc042330effc268f366f875007
Tomas Polach <tomas@polach.ch>
2017-09-27 17:39:58 +0200

    adds promise based cache to API calls receiving glTF URLs refactors for readability

--- src/a-frame/component/gblock.js
+++ src/a-frame/component/gblock.js
@@ -1,37 +1,18 @@
 // TODO: Replace placeholder shaders by original ones (requires fixing projection matrix)
+import Promise from 'bluebird'
+import fetchScript from '../../utils/io/fetch-script.js'
+import PromiseCache from '../../utils/promise-cache.js'
 import fragmentShader from './gblock/fragment-placeholder.glsl'
 import vertexShader from './gblock/vertex-placeholder.glsl'
 
-var GBlockLoader
-// THREE.GLTFLoader is required but not included in node.js version
-if (typeof THREE !== 'undefined' && THREE.GLTFLoader) {
+// configs
 
-  GBlockLoader = function GBlockLoader () {
+var LEGACY_GLFT_LOADER_URL = 'https://cdn.rawgit.com/mrdoob/three.js/r86/examples/js/loaders/GLTFLoader.js'
+var GBLOCK_API_GET_OFFICIAL_GLTF_URL = 'https://us-central1-gblock-api.cloudfunctions.net/get-gltf-url/?url='
 
-    THREE.GLTFLoader.call(this)
+// internals
 
-    var self = this
-
-    this._parse = this.parse
-    this.parse = function (data, path, onLoad, onError) {
-      // convert uint8 to json
-      var json = JSON.parse(convertUint8ArrayToString(data))
-      // use base64 shaders
-      Object.keys(json.shaders).forEach(function (key, i) {
-        // Replacing original shaders with placeholders
-        if (key.indexOf('fragment') > -1) json.shaders[key].uri = fragmentShader.base64
-        else if (key.indexOf('vertex') > -1) json.shaders[key].uri = vertexShader.base64
-      })
-      // convert json to uint8
-      var uint8array = new TextEncoder('utf-8').encode(JSON.stringify(json))
-      // parse data
-      self._parse.call(self, uint8array, path, onLoad, onError)
-    }
-
-  }
-  GBlockLoader.prototype = THREE.GLTFLoader.prototype
-
-}
+var promiseCache = new PromiseCache()
 
 // aframe module
 
@@ -40,9 +21,7 @@ export default {
   schema: {type: 'asset'},
 
   init: function () {
-    if (!GBlockLoader) throw 'gBlock component requires THREE.GLTFLoader'
     this.model = null
-    this.loader = new GBlockLoader()
   },
 
   update: function () {
@@ -59,36 +38,43 @@ export default {
     // The glTF itself is not being proxied and gets fetched from https://vr.google.com/downloads/* directly.
     // https://github.com/archilogic-com/aframe-gblock/issues/1
     // API server code: server/index.js
-    fetch('https://us-central1-gblock-api.cloudfunctions.net/get-gltf-url/?url=' + src).then(function (response) {
+    // try promise cache (could be in loading state)
+    var promiseFromCache = promiseCache.get(src)
+    var getGltfUrlPromise = promiseFromCache ? promiseFromCache : getGltfUrl(src)
+    if (!promiseFromCache) promiseCache.add(src, getGltfUrlPromise)
 
-      return response.json().then(function (message) {
+    getGltfUrlPromise.then(function (gltfUrl) {
 
-        if (!response.ok) {
-          el.emit('model-error', { message: message.message })
-          console.error('ERROR loading gblock model "'+ src +'" : ' + response.status + ' "' + message.message + '"')
-          return
-        }
+      // load glTF model from original URL
 
-        // load glTF model from original URL
-        var gltfUrl = message.gltfUrl
+      createModifiedGltfLoader().then(function(gltfLoader){
 
-        self.loader.load( gltfUrl, function onLoaded (gltfModel) {
+        gltfLoader.load( gltfUrl, function onLoaded (gltfModel) {
+
           self.model = gltfModel.scene || gltfModel.scenes[0]
-          // FIXME: adapt projection matrix in original shaders
+          // FIXME: adapt projection matrix in original shaders and do not replace materials
           self.model.traverse(function (child) {
             if (child.material) child.material = new THREE.MeshPhongMaterial({ vertexColors: THREE.VertexColors })
           })
           self.model.animations = gltfModel.animations
+
           el.setObject3D('mesh', self.model)
           el.emit('model-loaded', {format: 'gltf', model: self.model})
+
         }, function onProgress() {
           // do nothing
         }, function onError(error) {
-          el.emit('model-error', { message: error })
+
           console.error('ERROR loading gblock gltf "'+ src +'" : ' + error)
+          el.emit('model-error', { message: error })
+
         })
 
       })
+
+    }).catch(function(errorMessage){
+      console.error(errorMessage)
+      el.emit('model-error', { message: errorMessage })
     })
 
   },
@@ -100,7 +86,80 @@ export default {
 
 }
 
-// helpers
+// private shared methods
+
+function getGltfUrl (src) {
+
+  return fetch(GBLOCK_API_GET_OFFICIAL_GLTF_URL + src).then(function (response) {
+
+    // parse response
+    return response.json().then(function (message) {
+      if (response.ok) {
+        // return glTF URL
+        return message.gltfUrl
+      } else {
+        // handle error response
+        var status = response.status
+        var errorMessage = message.message
+        console.error('ERROR loading gblock model "'+ src +'" : ' + status + ' "' + errorMessage + '"')
+        return Promise.reject(errorMessage)
+      }
+    }).catch(function(error){
+      // handle server error
+      var errorMessage = '3dio.js: Error parsing gblock API server JSON response: ' + error
+      console.error(errorMessage)
+      return Promise.reject(errorMessage)
+    })
+
+  })
+
+}
+
+function createModifiedGltfLoader() {
+
+  // legacy loader will overwrite THREE.GLTFLoader so we need to keep reference to it
+  THREE.___OriginalGLTFLoader = THREE.GLTFLoader
+
+  return fetchScript(LEGACY_GLFT_LOADER_URL).then(function(){
+
+    // keep reference to fetched legacy loader
+    var LegacyGLTFLoader = THREE.GLTFLoader
+
+    // restore current GLTFLoader
+    THREE.GLTFLoader = THREE.___OriginalGLTFLoader
+
+    // create modified GLTF loader for google blocks
+    function GBlockLoader () {
+
+      LegacyGLTFLoader.call(this)
+
+      var self = this
+
+      this._parse = this.parse
+      this.parse = function (data, path, onLoad, onError) {
+        // convert uint8 to json
+        var json = JSON.parse(convertUint8ArrayToString(data))
+        // use base64 shaders
+        Object.keys(json.shaders).forEach(function (key, i) {
+          // Replacing original shaders with placeholders
+          if (key.indexOf('fragment') > -1) json.shaders[key].uri = fragmentShader.base64
+          else if (key.indexOf('vertex') > -1) json.shaders[key].uri = vertexShader.base64
+        })
+        // convert json to uint8
+        var uint8array = new TextEncoder('utf-8').encode(JSON.stringify(json))
+        // parse data
+        self._parse.call(self, uint8array, path, onLoad, onError)
+      }
+
+    }
+    GBlockLoader.prototype = LegacyGLTFLoader.prototype
+
+    // expose loader
+    return new GBlockLoader
+
+  })
+
+}
 
 // from https://github.com/mrdoob/three.js/blob/master/examples/js/loaders/GLTFLoader.js
 function convertUint8ArrayToString (array) {
