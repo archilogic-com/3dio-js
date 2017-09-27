3b5e95808544ad73b28d0515d4697b67a7cdb1c6
Benjamin Devine <benjamin.devine@gmail.com>
2017-09-27 13:52:15 +0200

    Use url from config

--- src/light/when-done.js
+++ src/light/when-done.js
@@ -2,29 +2,28 @@ import runtime from '../core/runtime.js'
 import poll from '../utils/poll.js'
 import fetch from '../utils/io/fetch.js'
 import urlUtils from '../utils/url.js'
+import configs from '../core/configs.js'
 
 // main
 
-export default function getBakeResult (processingId) {
+export default function getBakeResult(processingId) {
+  return poll(function(resolve, reject, next) {
+    var url = 'https://' + configs.storageDomainNoCdn + '/' + processingId
 
-  return poll(function(resolve, reject, next){
+    fetch(url)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(message) {
+        var status = message.params.status
 
-    var url = 'https://storage-nocdn.3d.io/' + processingId
-
-    fetch(url).then(function(response) {
-      return response.json()
-    }).then(function(message){
-      var status = message.params.status
-
-      if (status === 'ERROR') {
-        reject(message.params.data)
-      } else if (status === 'SUCCESS') {
-        resolve(message.params.data)
-      } else {
-        next()
-      }
-
-    })
+        if (status === 'ERROR') {
+          reject(message.params.data)
+        } else if (status === 'SUCCESS') {
+          resolve(message.params.data)
+        } else {
+          next()
+        }
+      })
   })
-
 }
