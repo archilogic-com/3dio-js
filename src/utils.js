6edbf32710d7dec7a5233b1ab97d730723d8dd68
Martin Splitt <mr.avgp@googlemail.com>
2017-10-13 23:49:40 +0300

    Adds removeFromCache method

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,7 @@ import cloneData3d                  from './utils/data3d/clone.js'
 import getData3dFromThreeJs         from './utils/data3d/from-three.js'
 import getData3dInspectorUrl        from './utils/data3d/get-inspector-url.js'
 import storeInCache                 from './utils/data3d/store-in-cache.js'
+import removeFromCache              from './utils/data3d/remove-from-cache.js'
 import ui                           from './utils/ui.js'
 import auth                         from './utils/auth.js'
 import fetch                        from './utils/io/fetch.js'
@@ -40,7 +41,8 @@ var utils = {
     clone: cloneData3d,
     traverse: traverseData3d,
     getInspectorUrl: getData3dInspectorUrl,
-    storeInCache: storeInCache
+    storeInCache: storeInCache,
+    removeFromCache: removeFromCache
   },
   ui: ui,
   auth: auth,
