99c6500c000eec4c0e479572ace00c62d6776abb
Tomas Polach <tomas@polach.ch>
2017-10-26 17:57:23 -0700

    removes jslint options

--- src/core/runtime/dom-ready.js
+++ src/core/runtime/dom-ready.js
@@ -3,10 +3,6 @@
  * Available via the MIT or new BSD license.
  * see: http://github.com/requirejs/domReady for details
  */
-/*jslint */
-/*global require: false, define: false, requirejs: false,
- window: false, clearInterval: false, document: false,
- self: false, setInterval: false */
 
 var isTop, testDiv, scrollIntervalId,
   isBrowser = typeof window !== "undefined" && window.document,
