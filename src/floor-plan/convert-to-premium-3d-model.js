7d42134584cb949cf393be9c4582d16fbdf532e3
Martin Splitt <mr.avgp@googlemail.com>
2017-12-06 00:13:11 +0100

    Exposing floorPlan.convertToPremium3dModel

--- src/floor-plan/convert-to-premium-3d-model.js
+++ src/floor-plan/convert-to-premium-3d-model.js
@@ -1,18 +1,25 @@
 import Promise from 'bluebird'
 import callServices from '../utils/services/call.js'
+import uuid from '../utils/uuid.js'
 
 export default function convertFloorPlanToPremium3dModel (args) {
 
   // API
   var floorPlan = args.floorPlan
+  var jobId = args.jobId
   var address = args.address
   var callback = args.callback
 
+  if (!jobId) jobId = uuid.generate()
+
   // send request to server side endpoint
   return callServices('FloorPlan.convertToPremium3dModel', {
-    floorplan: floorPlan,
-    address: address,
-    callback: callback
+    arguments: {
+      jobId: jobId,
+      floorplan: floorPlan,
+      address: address,
+      callback: callback
+    }
   }).then(function onSuccess (result) {
     // conversion request accepted
     return result.conversionId
