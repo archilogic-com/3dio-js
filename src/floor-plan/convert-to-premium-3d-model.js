04c23c06c6af31b512a805e1ad34a27dc4961605
Martin Splitt <mr.avgp@googlemail.com>
2017-12-05 16:47:28 +0100

    Adds floorplan.convertToPremium3dModel

---
+++ src/floor-plan/convert-to-premium-3d-model.js
@@ -0,0 +1,25 @@
+import Promise from 'bluebird'
+import callServices from '../utils/services/call.js'
+
+export default function convertFloorPlanToPremium3dModel (args) {
+
+  // API
+  var floorPlan = args.floorPlan
+  var address = args.address
+  var callback = args.callback
+
+  // send request to server side endpoint
+  return callServices('FloorPlan.convertToPremium3dModel', {
+    floorplan: floorPlan,
+    address: address,
+    callback: callback
+  }).then(function onSuccess (result) {
+    // conversion request accepted
+    return result.conversionId
+  }).catch(function onError (error) {
+    // conversion request error
+    // TODO: provide info in debug mode
+    return Promise.reject(error)
+  })
+
+}
