5c928bc65ec7a802fb83a8c930cce699959e5fa8
Martin Splitt <mr.avgp@googlemail.com>
2017-12-06 12:48:48 +0100

    Updating to 1.0.12

--- src/floor-plan.js
+++ src/floor-plan.js
@@ -1,11 +1,9 @@
 import convertToBasic3dModel from './floor-plan/convert-to-basic-3d-model.js'
-import convertToPremium3dModel from './floor-plan/convert-to-premium-3d-model.js'
 import getConversionStatus from './floor-plan/get-conversion-status.js'
 import recognize from './floor-plan/recognize.js'
 
 var floorPlan = {
   convertToBasic3dModel: convertToBasic3dModel,
-  convertToPremium3dModel: convertToPremium3dModel,
   getConversionStatus: getConversionStatus,
   recognize: recognize
 }
