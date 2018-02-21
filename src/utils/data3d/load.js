4cb6d769916e6f5893454cf7df1318b47f2d4f3f
dominic <gitkraken@mail.domi.ninja>
2018-02-21 12:43:39 +0100

    made loadData3d (url..) return promise for data3d.json urls as well

--- src/utils/data3d/load.js
+++ src/utils/data3d/load.js
@@ -10,23 +10,41 @@ var cache = new PromiseCache()
 
 export default function loadData3d (url, options) {
 
-  // prevent loading of unsupported formats
-  if (url.indexOf('data3d.buffer') < 0) return Promise.reject(url + ' no data3d')
-  // try cache
-  var cacheKey = url
-  var promiseFromCache = cache.get(cacheKey)
-  if (promiseFromCache) return promiseFromCache
+  // "prevent" loading of unsupported formats
+  if (url.match(/data3d\.buffer$/)) {
+    // try cache
+    var cacheKey = url
+    var promiseFromCache = cache.get(cacheKey)
+    if (promiseFromCache) return promiseFromCache
 
-  // fetch
-  var promise = fetch(url, options).then(function(res){
-    return res.arrayBuffer()
-  }).then(function(buffer){
-    return decodeBinary(buffer, { url: url })
-  })
+    // fetch
+    var promise = fetch(url, options).then(function(res){
+      return res.arrayBuffer()
+    }).then(function(buffer){
+      return decodeBinary(buffer, { url: url })
+    })
 
-  // add to cache
-  cache.add(cacheKey, promise)
+    // add to cache
+    cache.add(cacheKey, promise)
 
-  return promise
+    return promise
+  } else if (url.match(/data3d\.json$/)) {
+    // try cache
+    var cacheKey = url
+    var promiseFromCache = cache.get(cacheKey)
+    if (promiseFromCache) return promiseFromCache
+
+    // fetch
+    var promise = fetch(url, options).then(function(res){
+      return res
+    })
+
+    // add to cache
+    cache.add(cacheKey, promise)
+
+    return promise
+  } else {
+    return Promise.reject('\''+url+'\' does not end with data3d.buffer or data3d.json')
+  }
 
 }
