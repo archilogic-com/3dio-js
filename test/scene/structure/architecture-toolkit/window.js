30ea598bdaa9cfb73991d5f84ee2dccc74e930d2
dominic <gitkraken@mail.domi.ninja>
2018-04-19 18:55:37 +0200

    renamed to avoid namespace collision

--- test/scene/structure/architecture-toolkit/window.js
+++ test/scene/structure/architecture-toolkit/window.js
@@ -1,12 +1,12 @@
 import applyDefaults from '../../../../src/scene/structure/apply-defaults.js'
-import window from '../../../../src/scene/structure/parametric-objects/window'
+import window3d from '../../../../src/scene/structure/parametric-objects/window'
 
 // mock runtime module to prevent from tests blowing up
 jest.mock('../../../../src/core/runtime.js', () => ({isBrowser: false, isNode: true}))
 
 test('get window data3d', async function(){
   const attributes = applyDefaults({type: 'window'})
-  const data3d = await window(attributes)
+  const data3d = await window3d(attributes)
 
   // mesh names
   expect(Object.keys(data3d.meshes)).toEqual([ 'frame', 'glass' ])
