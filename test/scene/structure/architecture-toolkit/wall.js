421cbcdb60d37193a485abd77b10783b74a54087
dominic <dominic@archilogic.com>
2018-04-23 15:47:28 +0200

    fixed test runtime mock

--- test/scene/structure/architecture-toolkit/wall.js
+++ test/scene/structure/architecture-toolkit/wall.js
@@ -2,14 +2,14 @@ import applyDefaults from '../../../../src/scene/structure/apply-defaults.js';
 import wall from '../../../../src/scene/structure/parametric-objects/wall'
 
 // mock runtime module to prevent from tests blowing up
-jest.mock('../../../../src/core/runtime.js', () => ({isBrowser: false, isNode: true}))
+jest.mock('../../../../src/core/runtime.js', () => ({isBrowser: false, isNode: true, require: require}))
 
 test('get wall data3d', async function() {
   const attributes = applyDefaults({type: 'wall'})
   const data3d = await wall(attributes)
 
   // mesh names
-  expect(Object.keys(data3d)).toEqual([ 'front', 'back', 'top', 'base' ])
+  expect(Object.keys(data3d.meshes)).toEqual([ 'front', 'back', 'top', 'base' ])
   expect(data3d.meshes.front.positions).toBeDefined()
   expect(data3d.meshes.front.uvs).toBeDefined()
   expect(data3d.meshes.front.normals).toBeDefined()
