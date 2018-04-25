a6ced89a8e24d481dddc179c503307df67836b2e
Tomas Polach <tomas@polach.ch>
2018-04-25 20:05:27 +0200

    Release 1.1.10

--- build/3dio.js
+++ build/3dio.js
@@ -1,10 +1,10 @@
 /**
  * @preserve
  * @name 3dio
- * @version 1.1.8
- * @date 2018/02/22 02:41
- * @branch master
- * @commit 64153678b1a91d71bb941028d1d24a5f43edf093
+ * @version 1.1.10
+ * @date 2018/04/25 20:05
+ * @branch rollback
+ * @commit fe0641711febaf5335632f235122b438a55bd98b
  * @description toolkit for interior apps
  * @see https://3d.io
  * @tutorial https://github.com/archilogic-com/3dio-js
@@ -18,10 +18,10 @@
 	(global.io3d = factory());
 }(this, (function () { 'use strict';
 
-	var BUILD_DATE='2018/02/22 02:41', GIT_BRANCH = 'master', GIT_COMMIT = '64153678b1a91d71bb941028d1d24a5f43edf093'
+	var BUILD_DATE='2018/04/25 20:05', GIT_BRANCH = 'rollback', GIT_COMMIT = 'fe0641711febaf5335632f235122b438a55bd98b'
 
 	var name = "3dio";
-	var version = "1.1.8";
+	var version = "1.1.10";
 	var description = "toolkit for interior apps";
 	var keywords = ["3d", "aframe", "cardboard", "components", "oculus", "vive", "rift", "vr", "WebVR", "WegGL", "three", "three.js", "3D model", "api", "visualization", "furniture", "real estate", "interior", "building", "architecture", "3d.io"];
 	var homepage = "https://3d.io";
@@ -31,7 +31,7 @@
 	var main = "index.js";
 	var scripts = { "start": "gulp dev-browser", "dev-browser": "gulp dev-browser", "dev-node": "gulp dev-node", "jshint": "gulp jshint", "test": "jest --coverage", "build": "jest && gulp build", "release": "gulp release" };
 	var dependencies = { "bluebird": "^3.5.1", "form-data": "^2.3.1", "js-logger": "^1.4.1", "lodash": "^4.17.4", "node-fetch": "2.0.0-alpha.8", "pako": "^1.0.5", "performance-now": "^2.1.0", "rxjs": "^5.4.3", "three": "^0.85.2", "whatwg-fetch": "^2.0.3" };
-	var devDependencies = { "babel-core": "^6.26.0", "babel-jest": "^22.2.0", "babel-plugin-external-helpers": "^6.22.0", "babel-preset-env": "^1.6.1", "babel-preset-es2015": "^6.24.1", "babel-runtime": "^6.26.0", "chalk": "^2.1.0", "confirm-cli": "^0.4.0", "del": "^3.0.0", "gulp": "github:gulpjs/gulp#4.0", "gulp-git": "^2.4.2", "gulp-gzip": "^1.4.0", "gulp-jshint": "^2.0.4", "gulp-less": "^3.3.2", "gulp-s3": "0.11.0", "gulp-watch": "^4.3.11", "jest": "^22.2.1", "jshint": "^2.9.5", "jshint-stylish": "^2.2.1", "lite-server": "^2.3.0", "moment": "^2.19.0", "regenerator-runtime": "^0.11.1", "rollup": "^0.41.6", "rollup-plugin-babel": "^3.0.2", "rollup-plugin-commonjs": "^8.2.1", "rollup-plugin-json": "^2.3.0", "rollup-plugin-less": "^0.1.3", "rollup-plugin-node-resolve": "^3.0.0", "through2": "^2.0.3", "uglify-js": "^3.1.3" };
+	var devDependencies = { "babel-core": "^6.26.0", "babel-jest": "^22.2.0", "babel-plugin-external-helpers": "^6.22.0", "babel-preset-env": "^1.6.1", "babel-preset-es2015": "^6.24.1", "babel-runtime": "^6.26.0", "chalk": "^2.1.0", "confirm-cli": "^0.4.0", "del": "^3.0.0", "gulp": "github:gulpjs/gulp#4.0", "gulp-git": "^2.4.2", "gulp-gzip": "^1.4.0", "gulp-jshint": "^2.0.4", "gulp-less": "^3.3.2", "gulp-s3": "0.11.0", "gulp-watch": "^4.3.11", "jest": "^22.2.1", "jshint": "^2.9.5", "jshint-stylish": "^2.2.1", "lite-server": "^2.3.0", "moment": "^2.19.3", "regenerator-runtime": "^0.11.1", "rollup": "^0.41.6", "rollup-plugin-babel": "^3.0.2", "rollup-plugin-commonjs": "^8.2.1", "rollup-plugin-json": "^2.3.0", "rollup-plugin-less": "^0.1.3", "rollup-plugin-node-resolve": "^3.0.0", "through2": "^2.0.3", "uglify-js": "^3.1.3" };
 	var jest = { "collectCoverageFrom": ["src/**/*.{js,jsx}", "!**/node_modules/**", "!**/vendor/**"], "testMatch": ["**/test/**/*.js", "**/__tests__/**/*.js?(x)", "**/?(*.)(spec|test).js?(x)"] };
 	var packageJson = {
 		name: name,
@@ -35889,7 +35889,7 @@
 	  }).then(function (result) {
 	    return verifyResult(result, id, params);
 	  }).catch(function (error) {
-	    console.error(error);
+	    return Promise.reject(error);
 	  });
 	}
 
@@ -35924,7 +35924,7 @@
 	      };
 	    });
 	    return Promise.resolve(cleanResult);
-	  } else Promise.reject('No alternatives were found');
+	  } else return Promise.reject('No alternatives were found');
 	}
 
 	function getQuery(params) {
