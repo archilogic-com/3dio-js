d200e7762810e6334b163325986ef66cb1a677c7
Tomas Polach <tomas@polach.ch>
2018-04-26 13:03:06 +0200

    exit on jshint errors

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -7,4 +7,12 @@ module.exports = function runJshint () {
   return gulp.src('src/**/*.js')
     .pipe(jshint(jshintConfigs))
     .pipe(jshint.reporter(jshintReporter))
+    .pipe(jshint.reporter({
+      reporter: function(errors) {
+        if (errors.length) {
+          console.error(`Stopped after ${errors.length} JSHINT Errors.`)
+          process.exit(1)
+        }
+      }
+    }))
 }
