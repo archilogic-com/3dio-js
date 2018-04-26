dd7b146ebe18d85aa26ae06f50568034b2bfb660
dominic <dominic@archilogic.com>
2018-04-26 14:31:06 +0200

    added prototype data3d exporter

---
+++ src/scene/structure/parametric-objects/common/all.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+for file in $(ls ./src/scene/structure/parametric-objects/ | grep -v common | perl -pe 's/\.js//g'); do
+echo "import $file from '../$file'"
+done
+*/
+import closet from '../closet'
+import column from '../column'
+import door from '../door'
+import floor from '../floor'
+import kitchen from '../kitchen'
+import polyfloor from '../polyfloor'
+import railing from '../railing'
+import stairs from '../stairs'
+import wall from '../wall'
+import window3d from '../window3d'
+
+export default {
+  closet: closet,
+  column: column,
+  door: door,
+  floor: floor,
+  kitchen: kitchen,
+  polyfloor: polyfloor,
+  railing: railing,
+  wall: wall,
+  window3d: window3d,
+}
